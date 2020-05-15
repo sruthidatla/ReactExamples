54d1b678f78ba1fcc2c94a75433d24ce739d6798
dasr0001 <Sruthi.Datla.consultant@nielsen.com>
2020-05-15 14:07:37 +0530

    Added LazyLoading,Hooks and Error Boundaries example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React,{lazy,Suspense} from 'react';
 import './App.css';
-import ParentComponent from "./Components/ParentComponent";
-
+import ParentComponent from "./Components/PureComp/ParentComponent";
+import {HooksExample} from "./Components/HooksExample/HooksExample";
+import ErrorBoundary from "./Components/ErrorBoundariesExample/ErrorBoundary";
+import CounterExample from "./Components/ErrorBoundariesExample/CounterExample";
+const LazyComp = lazy(() => import("./Components/LazyLoadingExample/LazyComp"));
 
 function App() {
     console.log("rendering app")
   return (
     <div className="App">
-<ParentComponent/>
+{/*/!*<ParentComponent/>*!/*/}
+{/*<HooksExample/>*/}
+{/*        <Suspense fallback={<div>Loading.....</div>}>*/}
+{/*            <LazyComp />*/}
+{/*        </Suspense>*/}
+        <ErrorBoundary><CounterExample/></ErrorBoundary>
     </div>
   );
 }
---
+++ src/Components/ErrorBoundariesExample/ErrorBoundary.js
@@ -0,0 +1,44 @@
+import React,{Component} from 'react';
+
+class ErrorBoundary extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { error: null, errorInfo: null };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        console.log(error);
+        console.log(errorInfo);
+        this.setState({
+            error: error,
+            errorInfo: errorInfo
+        })
+
+    }
+    componentDidMount() {
+
+    }
+
+
+    
+
+    render() {
+        console.log(this.state.errorInfo+"from render of error boundary")
+        console.log(this.state.errorInfo?true:false)
+        if (this.state.errorInfo) {
+            return (
+                <div>
+                    <h2>Something went wrong.</h2>
+                    <div style={{ whiteSpace: 'pre-wrap' }}>
+                        {this.state.error && this.state.error.toString()}
+                        <br />
+                        {this.state.errorInfo[0]}
+                    </div>
+                </div>
+            );
+        }
+        console.log(this.props.children)
+        return this.props.children;
+    }
+}
+export default ErrorBoundary;
